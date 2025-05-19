a8cd4a44983b430c40b8bdc7d7afd446028d52e1
sagrawal <suresh.rp.agrawal@gmail.com>
2025-05-19 19:41:25 +0530

    added eager and lazy fetching methods

--- src/main/java/org/nsgacademy/Main.java
+++ src/main/java/org/nsgacademy/Main.java
@@ -9,11 +9,11 @@ import org.hibernate.cfg.Configuration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setRoll(555);
-        student.setName("siddhesh");
-        student.setMks(99.99f);
-        System.out.println(student);
+//        Student student = new Student();
+//        student.setRoll(555);
+//        student.setName("siddhesh");
+//        student.setMks(99.99f);
+//        System.out.println(student);
 
 //        Configuration config = new Configuration();
 //        config.addAnnotatedClass(org.nsgacademy.Student.class);
@@ -27,10 +27,26 @@ public class Main {
                             .addAnnotatedClass(org.nsgacademy.Student.class)
                             .buildSessionFactory();
 
+//        Session session = factory.openSession();
+//        Transaction transaction = session.beginTransaction();
+//        session.persist(student); //earlier in version 6, method was save()
+//        transaction.commit();
+
+
         Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(student);
-        transaction.commit();
+
+//        eager fetching : query will get executed whether we use it or not after fetching
+//        Student student = session.find(Student.class,111); //earlier in version 6, method was get()
+//        System.out.println(student);
+
+//        Student student = session.byId(Student.class).load(222); //another way to retrieve data by ID
+//        System.out.println(student);
+
+
+
+//        lazy fetching : if we dont print data after fetching then query will not get executed
+//        Student student = session.byId(Student.class).getReference(222); //another way to retrieve data by ID
+//        System.out.println(student);
 
         session.close();
         factory.close();
